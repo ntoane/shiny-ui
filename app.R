83994234f40882d83169a135e5aabbac15f0dd1d
Lekuba Ntoane <l.ntoane@gmail.com>
2023-01-11 18:11:16 +0200

    Added date inputs

--- app.R
+++ app.R
@@ -5,13 +5,19 @@ ui <- fluidPage(
     # Application title
     titlePanel("Shiny UI elements"),
     
+    # Text inputs
     textInput("name", "What is your name"),
     passwordInput("password", "Enter your password"),
     textAreaInput("story", "Tell me about yourself", rows=3),
     
+    # Numeric inputs
     numericInput("num", "Number one", value = 0, min = 0, max = 100),
     sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
-    sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100)
+    sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100),
+    
+    # Date inputs
+    dateInput("dob", "When were you born?"),
+    dateRangeInput("holiday", "When do you want to go on vacation next?")
 )
 
 # Define server logic required to draw a histogram
